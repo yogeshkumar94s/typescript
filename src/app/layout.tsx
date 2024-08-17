7b38cba1a87a3898cc15dd8af56e9aae3e2a24be
Yogesh Kumar <yksainimbd@gmail.com>
2024-08-17 23:19:13 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TypeScript: Learning",
+  description: "A simple project for learning typescript",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
